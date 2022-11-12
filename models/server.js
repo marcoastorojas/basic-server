af510741423cc2b80726bf2732a7b9af50de89d0
marco <you@example.com>
2022-11-12 11:55:10 -0500

    static

--- models/server.js
+++ models/server.js
@@ -17,6 +17,7 @@ class Server {
     }
 
     middlewares(){
+        this.app.use(express.static("public"))
         this.app.use(express.json())
         this.app.use(cors())
         this.routes()
